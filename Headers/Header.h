232266fc0842c6c7e935985d1b7378fc2a58e62a
Rudraksh Mishra <136945336+rudrakshm64@users.noreply.github.com>
2024-06-07 16:48:42 +0530

    Update Header.h
    Added new macros and header file include directive (stat.h)

--- Headers/Header.h
+++ Headers/Header.h
@@ -2,13 +2,20 @@
 #define HEADER
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <fcntl.h>
 #include <sys/sendfile.h>
 #include <unistd.h>
-#include <stdlib.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+#define TRUE 1
+#define FALSE 0
 
 #endif
