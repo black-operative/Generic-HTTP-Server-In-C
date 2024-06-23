3390560134d07a87dea82e6c23c36b6adac12c5b
Rudraksh Mishra <136945336+rudrakshm64@users.noreply.github.com>
2024-06-23 12:33:20 +0530

    Port Checking
    Added port availability checks and recompiled

--- Source/Server_Application.c
+++ Source/Server_Application.c
@@ -2,7 +2,9 @@
 #include "../Headers/Functions.h"
 
 int main(int argc, char const *argv[]) {
-    int PORT = 8080;
+    int PORT1 = 8080;
+    int PORT2 = 8888;
+    int CURRENT_PORT = 0;
 
     int server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -15,13 +17,24 @@ int main(int argc, char const *argv[]) {
     
     server_address.sin_family      = AF_INET;
     server_address.sin_addr.s_addr = INADDR_ANY;                     
-    server_address.sin_port        = htons(PORT);           //Port in hexadecimal. HTONS for small endian / little endian compatibility
+    server_address.sin_port        = htons(PORT1);           //Port in hexadecimal. HTONS for small endian / little endian compatibility
     
     if (bind(server_socket, &server_address, sizeof(server_address)) < 0) {
-        perror("Server socket binding error");
-        close(server_socket);
-        exit(EXIT_FAILURE);
-    } 
+        perror("Server socket binding error, trying another address");
+        
+        server_address.sin_family      = AF_INET;
+        server_address.sin_addr.s_addr = INADDR_ANY;                     
+        server_address.sin_port        = htons(PORT2);           //Port in hexadecimal. HTONS for small endian / little endian compatibility
+
+        if (bind(server_socket, &server_address, sizeof(server_address)) < 0) {
+            perror("Server socket binding error");
+            close(server_socket);
+            exit(EXIT_FAILURE);
+        } 
+        else { CURRENT_PORT = PORT2; }
+    } else {
+        CURRENT_PORT = PORT1;
+    }
 
     if (listen(server_socket, 10) < 0) {
         perror("Server listen error");
@@ -29,9 +42,8 @@ int main(int argc, char const *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-
-    printf("Server is now Running at port : %d\n", PORT);
-    printf("Website available at : localhost:%d\n", PORT);
+    printf("Server is now Running at port : %d\n", CURRENT_PORT);
+    printf("Website available at : localhost:%d\n", CURRENT_PORT);
     
     while (TRUE) {
         int client_socket = accept(server_socket, 0, 0);
