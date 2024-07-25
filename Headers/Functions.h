59d8ba084b0ea2e8642feb2c46ed509c2efb22ac
Rudraksh Mishra <136945336+rudrakshm64@users.noreply.github.com>
2024-07-25 20:30:13 +0530

    Changed return type for functions
    void -> int
    For Error handling using status codes and code flow control

--- Headers/Functions.h
+++ Headers/Functions.h
@@ -3,9 +3,9 @@
 #define FUNCTIONS
 
 void handle_client(int client_socket);
-void send_response(int client_socket, const char *header, const char *content_type, const char *body, int body_length);
-void send_file_res(int client_socket, const char *path);
-void not_found_res(int client_socket);
+int  send_response(int client_socket, const char *header, const char *content_type, const char *body, int body_length);
+int  send_file_res(int client_socket, const char *path);
+int  not_found_res(int client_socket);
 
 #define STATUS_OK  "HTTP/1.1 200 OK\r\n"
 #define STATUS_ERR "HTTP/1.1 404 NOT FOUND\r\n"
