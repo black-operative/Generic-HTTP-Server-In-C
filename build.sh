2ea2ad8473e8160f8578a93a90ebe45a634edfae
Rudraksh Mishra <136945336+rudrakshm64@users.noreply.github.com>
2024-08-10 11:32:26 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
 
-# Set the directories
 INCLUDE_DIR=./Headers
 SRC_DIR=./Source
 BUILD_DIR=./Build
 
-# Set the output executable name
 OUTPUT=$BUILD_DIR/App
 
-# Create the build directory if it doesn't exist
-mkdir -p $BUILD_DIR
-
-# Compile the source files
-gcc -I$INCLUDE_DIR -c $SRC_DIR/Server_Application.c -o $BUILD_DIR/Server_Application.o
-gcc -I$INCLUDE_DIR -c $SRC_DIR/Functions.c -o $BUILD_DIR/Functions.o
-
-# Link the object files into an executable
+gcc -I $INCLUDE_DIR -c $SRC_DIR/Server_Application.c -o $BUILD_DIR/Server_Application.o
+gcc -I $INCLUDE_DIR -c $SRC_DIR/Functions.c -o $BUILD_DIR/Functions.o
 gcc $BUILD_DIR/Server_Application.o $BUILD_DIR/Functions.o -o $OUTPUT
 
-# Print a success message
-echo "Build successful. Executable created: $OUTPUT"
+echo "Build successful!"
